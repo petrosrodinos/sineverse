271adf783cd642ee3bec74f7c4b542d3d9133be5
Petros <petros.rodinos@yahoo.com>
2026-04-26 09:24:37 +0300

    api

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { message: string; timestamp: string } {
+    return {
+      message: 'Hello World!',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
